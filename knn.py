dd19720386080f6c5584b514839308aa0f49df35
Daniel Davis <dan@danizen.net>
2017-03-31 00:26:24 -0400

    Section 2.3

--- knn.py
+++ knn.py
@@ -1,6 +1,8 @@
 import numpy as np
 import operator
 import pandas as pd
+import glob
+from os.path import basename
 
 
 def create_dataset():
@@ -68,4 +70,53 @@ def datingClassTest(filename):
      
     return (num_errors/float(num_test_rows))
 
+
+def img2vector(filename):
+    vector = np.zeros((1,1024))
+    f = open(filename)
+    for i in range(32):
+        l = f.readline()
+        for j in range(32):
+            vector[0,32*i + j] = int(l[j])
+    return vector
+
+
+def handwritingLoadTrain(traindir):
+    """
+    Load trainingDigits files
+    """
+    labels = []
+    trainfiles = glob.glob('%s/*.txt' % traindir)
+    m = len(trainfiles)
+    train_matrix = np.zeros((m, 1024))
+    for i in range(m):
+        thisfile = trainfiles[i]
+        thislabel = int(basename(thisfile).split('_')[0])
+        labels.append(thislabel)
+        train_matrix[i,:] = img2vector(thisfile)
+
+    return train_matrix, labels
+
+
+def handwritingClassTest(k, traindir = 'data/ch02/trainingDigits', testdir = 'data/ch02/testDigits'):
+
+    train_matrix, labels = handwritingLoadTrain(traindir)
+
+    testfiles = glob.glob('%s/*.txt' % testdir)
+    mtest = len(testfiles)
+    errorCount = 0
+
+    for i in range(mtest):
+        thisfile = testfiles[i]
+        actualClass = int(basename(thisfile).split('_')[0])
+        testvec = img2vector(thisfile)
+
+        predictedClass = classify0(testvec,
+                                   train_matrix, labels, k)
+        if actualClass != predictedClass:
+            errorCount += 1
+            args = (basename(thisfile), actualClass, predictedClass)
+            print('%s: class sb %d is %d' % args)
+    
+    return errorCount, float(errorCount)/float(mtest)
 
