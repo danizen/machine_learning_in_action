31be086041c8dfdcbc7a1fe993d1427e0a1a2a53
Daniel Davis <dan@danizen.net>
2017-04-04 21:57:53 -0400

    Chapter 3.

--- dectre.py
+++ dectre.py
@@ -113,3 +113,17 @@ def createPlot(atree):
     plotTree(atree, (0.5, -0.2), '')
     plt.show()
 
+
+def classify(inputTree, featLabels, testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
+
